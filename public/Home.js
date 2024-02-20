2de6443f852684b2f6eec7a0f8b2b0e2972a3113
Thaipat Lawthanavijit <88415624+Thaipat@users.noreply.github.com>
2024-02-20 07:34:29 +0700

    Update Home.js

--- public/Home.js
+++ public/Home.js
@@ -97,6 +97,140 @@ function createRoom() {
     })
 }
 
+tutorialImg = [`https://media.discordapp.net/attachments/986985862631915541/1209050641763278869/image.png?ex=65e58328&is=65d30e28&hm=f5d89357cdc0fbfbde061ed2534f4d5524b05ffd3876daa88259138a45cd06fe&=&format=webp&quality=lossless&width=982&height=451`, `https://media.discordapp.net/attachments/986985862631915541/1209050876996751430/image.png?ex=65e58360&is=65d30e60&hm=ff3228e9dd3a5303246a99fee2d976a2e3f0958f1aa05b790adea4fd87b0b77b&=&format=webp&quality=lossless&width=981&height=457`, `https://media.discordapp.net/attachments/986985862631915541/1209050945049067560/image.png?ex=65e58370&is=65d30e70&hm=7ca15c654a8b05f8c56ed0e445990d7c456258f2811965c0bffa36b7e75e2958&=&format=webp&quality=lossless&width=687&height=321`, `https://media.discordapp.net/attachments/986985862631915541/1209051011679912017/image.png?ex=65e58380&is=65d30e80&hm=657203c24c8a74a90014f05ad8337d09dbfdb94c358f2a94172d04b8d6f109b7&=&format=webp&quality=lossless&width=687&height=321`, `https://media.discordapp.net/attachments/986985862631915541/1209051065371336704/image.png?ex=65e5838d&is=65d30e8d&hm=bc4352646e90bcf79626e5e0eb2e3db22568e414e260e42aee8d45c74da88d58&=&format=webp&quality=lossless&width=687&height=323`, `https://media.discordapp.net/attachments/986985862631915541/1209051140205842502/image.png?ex=65e5839e&is=65d30e9e&hm=b8628417de89585daee84c9b302c0617b4ea51e336d41c758859eb81f43cf5af&=&format=webp&quality=lossless&width=687&height=322`]
+
+let tutorialImgCount = 0
+
 function tutorial(){
-    console.log("test")
+    document.querySelector("#tutorial").innerHTML = `<div class="relative flex justify-center items-center z-10"
+    style="width:100vw; height:100vh; background-color: rgba(1,0,0,0.5);">
+    <div class="relative flex justify-center items-center btn-color" style="width: 70vw; height: 90vh;">
+        <div class="text-center">
+                <div class="flex justify-center mt-4">
+                    <img style="width: 70vw;" src="${tutorialImg[tutorialImgCount]}">
+                </div>
+            <div class="relative flex justify-center items-center m-4">
+                <button class="tutorial-btn flex justify-center items-center btn-color" onclick="closeTutorial()">
+                    <span class="textstroke" style="font-size: 35px;">Close</span>
+                </button>
+                <div class="tutorial-btn flex justify-center items-center btn-color"
+                style="margin-left: 10vw; margin-right: 10vw;">
+                    <span class="textstroke" style="font-size: 35px;">
+                        <span id="tutorial-page">${tutorialImgCount+1}</span>/6
+                    </span>
+                </div>
+                <button class="tutorial-btn flex justify-center items-center btn-color" onclick="nextTutorial()">
+                    <span class="textstroke relative" style="font-size: 35px;">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+    </div>`
+}
+
+function nextTutorial(){
+    tutorialImgCount += 1
+    tutorialImgCount == 5 ? document.querySelector("#tutorial").innerHTML = `<div class="relative flex justify-center items-center z-10"
+    style="width:100vw; height:100vh; background-color: rgba(1,0,0,0.5);">
+    <div class="relative flex justify-center items-center btn-color" style="width: 70vw; height: 90vh;">
+        <div class="text-center">
+                <div class="flex justify-center mt-4">
+                    <img style="width: 70vw;" src="${tutorialImg[tutorialImgCount]}">
+                </div>
+            <div class="relative flex justify-center items-center m-4">
+                <button class="tutorial-btn flex justify-center items-center btn-color" onclick="backTutorial()">
+                    <span class="textstroke" style="font-size: 35px;">Back</span>
+                </button>
+                <div class="tutorial-btn flex justify-center items-center btn-color"
+                style="margin-left: 10vw; margin-right: 10vw;">
+                    <span class="textstroke" style="font-size: 35px;">
+                        <span id="tutorial-page">${tutorialImgCount+1}</span>/6
+                    </span>
+                </div>
+                <button class="tutorial-btn flex justify-center items-center btn-color" onclick="closeTutorial()">
+                    <span class="textstroke relative" style="font-size: 35px;">Close</span>
+                </button>
+            </div>
+        </div>
+    </div>
+    </div>` : document.querySelector("#tutorial").innerHTML = `<div class="relative flex justify-center items-center z-10"
+    style="width:100vw; height:100vh; background-color: rgba(1,0,0,0.5);">
+    <div class="relative flex justify-center items-center btn-color" style="width: 70vw; height: 90vh;">
+        <div class="text-center">
+                <div class="flex justify-center mt-4">
+                    <img style="width: 70vw;" src="${tutorialImg[tutorialImgCount]}">
+                </div>
+            <div class="relative flex justify-center items-center m-4">
+                <button class="tutorial-btn flex justify-center items-center btn-color" onclick="backTutorial()">
+                    <span class="textstroke" style="font-size: 35px;">Back</span>
+                </button>
+                <div class="tutorial-btn flex justify-center items-center btn-color"
+                style="margin-left: 10vw; margin-right: 10vw;">
+                    <span class="textstroke" style="font-size: 35px;">
+                        <span id="tutorial-page">${tutorialImgCount+1}</span>/6
+                    </span>
+                </div>
+                <button class="tutorial-btn flex justify-center items-center btn-color" onclick="nextTutorial()">
+                    <span class="textstroke relative" style="font-size: 35px;">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+    </div>`
+}
+
+function backTutorial(){
+    tutorialImgCount -= 1
+    tutorialImgCount == 0 ? document.querySelector("#tutorial").innerHTML = `<div class="relative flex justify-center items-center z-10"
+    style="width:100vw; height:100vh; background-color: rgba(1,0,0,0.5);">
+    <div class="relative flex justify-center items-center btn-color" style="width: 70vw; height: 90vh;">
+        <div class="text-center">
+                <div class="flex justify-center mt-4">
+                    <img style="width: 70vw;" src="${tutorialImg[tutorialImgCount]}">
+                </div>
+            <div class="relative flex justify-center items-center m-4">
+                <button class="tutorial-btn flex justify-center items-center btn-color" onclick="closeTutorial()">
+                    <span class="textstroke" style="font-size: 35px;">close</span>
+                </button>
+                <div class="tutorial-btn flex justify-center items-center btn-color"
+                style="margin-left: 10vw; margin-right: 10vw;">
+                    <span class="textstroke" style="font-size: 35px;">
+                        <span id="tutorial-page">${tutorialImgCount+1}</span>/6
+                    </span>
+                </div>
+                <button class="tutorial-btn flex justify-center items-center btn-color" onclick="nextTutorial()">
+                    <span class="textstroke relative" style="font-size: 35px;">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+    </div>` : document.querySelector("#tutorial").innerHTML = `<div class="relative flex justify-center items-center z-10"
+    style="width:100vw; height:100vh; background-color: rgba(1,0,0,0.5);">
+    <div class="relative flex justify-center items-center btn-color" style="width: 70vw; height: 90vh;">
+        <div class="text-center">
+                <div class="flex justify-center mt-4">
+                    <img style="width: 70vw;" src="${tutorialImg[tutorialImgCount]}">
+                </div>
+            <div class="relative flex justify-center items-center m-4">
+                <button class="tutorial-btn flex justify-center items-center btn-color" onclick="backTutorial()">
+                    <span class="textstroke" style="font-size: 35px;">Back</span>
+                </button>
+                <div class="tutorial-btn flex justify-center items-center btn-color"
+                style="margin-left: 10vw; margin-right: 10vw;">
+                    <span class="textstroke" style="font-size: 35px;">
+                        <span id="tutorial-page">${tutorialImgCount+1}</span>/6
+                    </span>
+                </div>
+                <button class="tutorial-btn flex justify-center items-center btn-color" onclick="nextTutorial()">
+                    <span class="textstroke relative" style="font-size: 35px;">Next</span>
+                </button>
+            </div>
+        </div>
+    </div>
+    </div>`
+}
+
+function closeTutorial(){
+    tutorialImgCount = 0
+    document.querySelector("#tutorial").innerHTML = ""
 }
